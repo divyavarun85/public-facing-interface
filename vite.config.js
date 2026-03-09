5cba48df61fbb726884075e8787597db259589f9
Divya Varun <divyavarun85@gmail.com>
2026-03-09 12:19:31 -0400

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/public-facing-interface/',
   plugins: [vue()],
   build: {
     // Enable code splitting
